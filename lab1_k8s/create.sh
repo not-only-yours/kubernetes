648f5fe422e68dcf1379412befbdb36e5050deb8
not-only-yours <darty.vis@gmail.com>
2022-06-14 22:52:08 +0300

    fixed files lab1

--- lab1_k8s/create.sh
+++ lab1_k8s/create.sh
@@ -2,5 +2,3 @@ docker build -t client:0.1 -f client/Dockerfile .
 docker build -t service1:0.1 -f services/service1/Dockerfile .
 docker build -t service2:0.1 -f services/service2/Dockerfile .
 kubectl apply -f k8s/ingress.yaml
-kubectl apply -f k8s/service2/service2-service.yaml
-kubectl apply -f k8s/service1/service1-service.yaml
