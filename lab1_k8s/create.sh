9fb4070aa6988d6309eeb1eefe31bb443f271b30
not-only-yours <darty.vis@gmail.com>
2022-06-14 22:15:53 +0300

    changed files

---
+++ lab1_k8s/create.sh
@@ -0,0 +1,6 @@
+docker build -t client:0.1 -f client/Dockerfile .
+docker build -t service1:0.1 -f services/service1/Dockerfile .
+docker build -t service2:0.1 -f services/service2/Dockerfile .
+kubectl apply -f k8s/service1/service1-deployment.yaml
+kubectl apply -f k8s/service2/service2-deployment.yaml
+kubectl apply -f k8s/client/service1-deployment.yaml
